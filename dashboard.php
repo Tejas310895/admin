64e3238ee27b313b669c6ddace79671b9a677966
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2023-12-01 23:21:12 +0530

    'changes'

--- dashboard.php
+++ dashboard.php
@@ -93,16 +93,19 @@
             <div class="card-header py-3">
                 <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
             </div>
-            <?php foreach (get_project_progress() as $vals) {
-                $progress_ind = round(($vals['prog'] / $vals['total_prog']) * 100);
+            <?php
+            if (!empty(get_project_progress())) {
+                foreach (get_project_progress() as $vals) {
+                    $progress_ind = (round(($vals['prog'] / $vals['total_prog']) * 100));
             ?>
-                <div class="card-body">
-                    <h4 class="small font-weight-bold"><?php echo $vals['proj_id']; ?> <span class="float-right"><?php echo $progress_ind; ?>%</span></h4>
-                    <div class="progress mb-4">
-                        <div class="progress-bar bg-<?php echo ($progress_ind < 30) ? 'danger' : (($progress_ind < 50) ? 'warning' : 'success'); ?>" role="progressbar" style="width: <?php echo $progress_ind ?>%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="card-body">
+                        <h4 class="small font-weight-bold"><?php echo $vals['proj_id']; ?> <span class="float-right"><?php echo $progress_ind; ?>%</span></h4>
+                        <div class="progress mb-4">
+                            <div class="progress-bar bg-<?php echo ($progress_ind < 30) ? 'danger' : (($progress_ind < 50) ? 'warning' : 'success'); ?>" role="progressbar" style="width: <?php echo $progress_ind ?>%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
                     </div>
-                </div>
-            <?php } ?>
+            <?php }
+            } ?>
         </div>
     </div>
 </div>
